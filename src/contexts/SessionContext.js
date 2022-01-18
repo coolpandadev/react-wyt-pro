32edb0590075af44f223be239cebc76cfe9bd6c3
coolpandadev <coolpanda1219@gmail.com>
2022-01-18 19:42:43 +0800

    Updated Session Context

--- src/contexts/SessionContext.js
+++ src/contexts/SessionContext.js
@@ -6,23 +6,24 @@ export const SessionContext = createContext();
 const SessionContextProvider = ({children}) => {
     
     //These are the states we're initializing to store component data related to user Login/Authentication status and their authentication credentials
-    const [isMsgListLoading, setIsMsgListLoading] = useState(true)
-    useEffect(() => {
-        // if (msgList?.length) {
-            // setIsMsgListLoading(false)
-        // }
-        const timer = setTimeout(() => {
-            setIsMsgListLoading(false)
-        }, 1000)
-        return ()=>{clearTimeout(timer)}
-    },[msgList])
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const setLoggedIn = (bool) => {
+        setIsLoggedIn(bool)
+    }
     
+    //Pull LoggedInstate data from local storage and set current states to that on mount
+    useEffect(()=>{
+        setLoggedIn(JSON.parse(sessionStorage.getItem('isLoggedIn')));
+    },[])
 
     const props = {
+        isLoggedIn,
+        setLoggedIn
     }
 
     return (
-        //We add the Provider property from the created AuthContext object which will provide all children with necessary values/states without
+        //We add the Provider property from the created SessionContext object which will provide all children with necessary values/states without
         //passing them directly as props. In this case, we pass our user login states and updaters to the value prop of Provider,
         //so we can access these in children components using useContext
         <SessionContext.Provider value={props}>
