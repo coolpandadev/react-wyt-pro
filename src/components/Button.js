bad43a6756d18c44ae9616bd1bd4055e7aece400
coolpandadev <coolpanda1219@gmail.com>
2022-01-27 00:26:42 +0800

    Added CSS to leagues and trades page. Added team_logo property as data sent to /trades post

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({cb, color, children}) => {
+const Button = ({cb, color, children, classnames}) => {
   return (
-    <button onClick={cb} className={`${color} rounded-full py-1 px-4`}>
+    <button onClick={cb} className={`${color} ${classnames} rounded-md py-1 px-4 font-header uppercase`}>
         {children}
     </button>
   )
