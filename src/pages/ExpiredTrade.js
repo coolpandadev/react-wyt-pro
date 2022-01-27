bbb6dc2c9f83dc3d5b125583f060cded55114d4c
coolpandadev <coolpanda1219@gmail.com>
2022-01-27 20:29:27 +0800

    Update ExpiredTrade.js

--- src/pages/ExpiredTrade.js
+++ src/pages/ExpiredTrade.js
@@ -10,10 +10,10 @@ function ExpiredTrade() {
         return navigate(`/`)
     }
     return <div>
-        <div className='w-screen h-screen flex flex-col justify-center items-center gap-3 '>
+        <div className='w-screen h-screen flex flex-col justify-center items-center gap-3 px-5 sm:px-[5em] '>
             <img className='w-[60%] h-[400px]' src={greekfreek} />
-            <h1 className='font-body font-bold text-[3em]'>Oh no!</h1>
-            <p className='font-body text-[1.5em]'>We are sorry for the inconvenience! It looks like you are trying to access a trade that has been deleted or an error on our database</p>
+            <h1 className='font-body font-bold text-[3em] md:text-[4em]'>Oh no!</h1>
+            <p className='font-body text-[1.5em] sm:text-center md:text-[1.8em]'>We are sorry for the inconvenience! It looks like you are trying to access a trade that has been deleted or an error on our database</p>
             <button onClick={() => handleBack()} className='bg-emerald-500 px-3 py-2 rounded-md font-body text-[1.5em]'>Back to Home</button>
 
         </div>
